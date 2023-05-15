c64ec802604f712fe29b13d909d1c8cad856640d
alexandru.patroi <alexandru.patroi@opswat.com>
2023-05-15 14:57:34 +0300

    create sg

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
+variable "my_ip" {} #trebuie sa adaug si adresea de acasa
 
 
 resource "aws_vpc" "myapp-vpc" {
@@ -45,4 +46,33 @@ resource "aws_route_table" "myapp-route-table" {
 resource "aws_route_table_association" "a-rtb-subnet" {
   subnet_id = aws_subnet.myapp-subnet-1.id
   route_table_id = aws_route_table.myapp-route-table.id
+}
+
+
+resource "aws_security_group" "myapp-sg" {
+  name="myapp-sg"  
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  ingress {  # this is for nginx
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+}
+tags = {
+    Name: "${var.env_prefix}-sg"
+  }
 }
